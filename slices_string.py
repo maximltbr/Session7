3683f4bdcc2573631c169710c63140fa3a7dc0e8
Maxim-Lichtenberger <mlichtenberg.ieu2023@student.ie.edu>
2025-01-31 12:26:43 +0100

    Added string slices

---
+++ slices_string.py
@@ -0,0 +1,18 @@
+s = "abcdefghijklmnop"
+print(s)
+print(s[1:4], s[6:9])
+print(s[:4], s[6:])
+print(s[1:10:2])
+print(s[::-1])
+print("racecar"[::-1])
+
+# This is how you replace a letter
+s ="cat"
+s = "r" + s[1:]
+print(s)
+
+s ="seven"
+s = s[:2] + "7" + s[3:]
+print(s)
+
+
