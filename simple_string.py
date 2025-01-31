8a3a3f4c74600116a481bc33be16223574d88289
Maxim-Lichtenberger <mlichtenberg.ieu2023@student.ie.edu>
2025-01-31 11:24:34 +0100

    string examples

---
+++ simple_string.py
@@ -0,0 +1,12 @@
+s = "Hello"
+print(s)
+s2 = 'Hello sir #@#ä2e23'
+print (s2)
+s = ('And she said: "hello!"')
+print(s)
+print(",.'\"\"\"\"")
+s = "hello"
+print(s[2], s[4])
+print(s[-1])
+print(s[1], s[-5])
+
